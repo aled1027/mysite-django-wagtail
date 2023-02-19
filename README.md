723a2b93571a2e5995e87305fb2c69a6779d7b61
Alex Ledger <alex@playgroundlabs.io>
2023-02-19 09:29:03 -0800

    Worked through tutorial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# a-blog
+# mysite
+
+https://docs.wagtail.org/en/stable/getting_started/tutorial.html
+
+```bash
+wagtail start mysite mysite
+```
+
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-w15si@sf847uk98zt-2*0knfalxw$px=tecx)8u$)%mp_by-ro"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
